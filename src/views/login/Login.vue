889cc4a1507106427971bff3ac68749216449a5c
strLu <str_lu@163.com>
2019-06-24 16:20:49 +0800

    使用第三方插件’极验‘人机交互验证，增加登录短信验证功能

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,33 +1,44 @@
 <template>
-    <div class="login">
-        <div class="form-wrap">
-            <div class="form-logo">
-                <img src="./logo_index.png" alt="啦啦啦">
-            </div>
-            <el-form :model="form" :rules="rules" ref="form" class="form-content">
-                <el-form-item  prop="mobile">
-                    <el-input v-model="form.mobile" placeholder="输入手机号"></el-input>
-                </el-form-item>
-                <el-form-item  prop="code">
-                    <el-col :span="11">
-                        <el-input v-model="form.code" placeholder="输入验证码"></el-input>
-                    </el-col>
-                    <el-col :offset="2" :span="11">
-                        <el-button class="sendCode" @click="handleSendCode">发送验证码</el-button>
-                    </el-col>
-                </el-form-item>
-                <el-form-item prop="checked">
-                    <el-checkbox class="agree-checkbox" v-model="form.checked"></el-checkbox>
-                    <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></span>
-                </el-form-item>
-                <el-form-item>
-                    <el-button class="loginbtn" type="primary" @click="handleLogin" >立即登录</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="login">
+    <div class="form-wrap">
+      <div class="form-logo">
+        <img src="./logo_index.png" alt="啦啦啦">
+      </div>
+      <el-form :model="form" :rules="rules" ref="form" class="form-content">
+        <el-form-item prop="mobile">
+          <el-input v-model="form.mobile" placeholder="输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-col :span="11">
+            <el-input v-model="form.code" placeholder="输入验证码"></el-input>
+          </el-col>
+          <el-col :offset="2" :span="11">
+            <el-button
+              :disabled="!!codeTimer"
+              @click="handleSendCode"
+            >{{ codeTimer ? `${codeTimerSeconds}秒后重试`:'获取验证码' }}</el-button>
+          </el-col>
+        </el-form-item>
+        <el-form-item prop="checked">
+          <el-checkbox class="agree-checkbox" v-model="form.checked"></el-checkbox>
+          <span class="agree-text">
+            我已阅读并同意
+            <a href="#">用户协议</a>和
+            <a href="#">隐私条款</a>
+          </span>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="loginbtn" type="primary" @click="handleLogin">立即登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 <script>
+import axios from 'axios'
+import '@/vendor/gt.js'
+const initCodeTimeSecond = 5
+
 export default {
   data () {
     return {
@@ -39,67 +50,163 @@ export default {
       rules: {
         mobile: [
           { required: true, message: '请输入手机号', trigger: 'blur' },
-          { pattern: /^\d{11}$/, message: '请输入11位手机号', trigger: 'blur' }
+          {
+            pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/,
+            message: '请输入11位的有效手机号',
+            trigger: 'blur'
+          }
         ],
         code: [
           { required: true, message: '请输入验证码', trigger: 'blur' },
-          { pattern: /^\d{6}$/, message: '请输入6位数字验证码', trigger: 'blur' }
+          {
+            pattern: /^\d{6}$/,
+            message: '请输入6位数字验证码',
+            trigger: 'blur'
+          }
         ],
         checked: [
           { pattern: /true/, message: '请阅读并同意用户协议和隐私条款' }
         ]
-      }
+      },
+      codeTimer: null,
+      codeTimerSeconds: initCodeTimeSecond
     }
   },
   methods: {
     handleSendCode () {
-
+      // 验证手机号是否有效
+      this.$refs['form'].validateField('mobile', errorMessage => {
+        if (errorMessage.trim().length > 0) {
+          return null
+        }
+        // 验证通过，执行初始化验证码操作
+        this.initCode()
+      })
+    },
+    initCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        const { data } = res.data
+        window.initGeetest({
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind' // 验证模式  弹出式
+        }, captchaObj => {
+          captchaObj.onReady(() => {
+            captchaObj.verify()// 弹出验证码内容框
+          }).onSuccess(() => {
+            const {
+              geetest_challenge: challenge,
+              geetest_seccode: seccode,
+              geetest_validate: validate
+            } = captchaObj.getValidate()
+            axios({
+              method: 'GET',
+              url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+              params: {
+                challenge,
+                validate,
+                seccode
+              }
+            }).then(res => {
+              // 成功发送短信，执行倒计时
+              this.countDown()
+            })
+            console.log('成功发送短信')
+          }).onError(() => {
+            console.log('发送短信出错了')
+          })
+        })
+      })
+    },
+    // 倒计时
+    countDown () {
+      this.codeTimer = setInterval(() => {
+        this.codeTimerSeconds--
+        if (this.codeTimerSeconds <= 0) {
+          window.clearInterval(this.codeTimer)
+          this.codeTimerSeconds = initCodeTimeSecond
+          this.codeTimer = null
+        }
+      }, 1000)
     },
     handleLogin () {
-      this.$refs['form'].validate(valid => {
-        if (!valid) {
-
+      this.$refs['form'].validate(error => {
+        if (!error) {
+          return null
+        }
+        // 表单验证通过，提交登录请求
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: {
+          mobile: this.form.mobile,
+          code: this.form.code
         }
       })
+        .then(res => {
+          // 登录成功  200<= 的状态会进入这里 <=400
+          this.$message({
+            message: '登录成功',
+            type: 'success'
+          })
+          this.$router.push({
+            name: 'home'
+          })
+        })
+        .catch(() => {
+          // >=400 的状态会进入这里
+          this.$message.error('登录失败，手机号或验证码错误！')
+        })
     }
   }
 }
 </script>
 <style lang="less" scoped>
-.login{
-    height: 100%;
-    background: url(./login_bg.jpg)no-repeat;
-    .form-wrap{
-        position:absolute;
-        left:0;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        width: 260px;
-        height: 320px;
-        padding: 30px;
-        border: 1px solid #ccc;
-        margin:auto;
-        background: #fff;
-        .form-logo {
-            padding: 0 10px 30px;
-            img{
-                width: 100%;
-            }
-        }
-        .loginbtn{
-            width: 100%;
-        }
-        .sendCode{
-            float: right;
-        }
-        .agree-checkbox{
-            margin-right: 10px;
-        }
-        .agree-text a{
-            color: #409eff;
-            text-decoration: none;
-        }
+.login {
+  height: 100%;
+  background: url(./login_bg.jpg) no-repeat;
+  background-size: cover;
+  .form-wrap {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 270px;
+    height: 320px;
+    padding: 30px;
+    border: 1px solid #ccc;
+    margin: auto;
+    background: #fff;
+    .form-logo {
+      padding: 0 10px 30px;
+      img {
+        width: 100%;
+      }
     }
+    .loginbtn {
+      width: 100%;
+    }
+    .sendCode {
+      float: right;
+    }
+    .agree-checkbox {
+      margin-right: 10px;
+    }
+    .agree-text a {
+      color: #409eff;
+      text-decoration: none;
+    }
+  }
 }
 </style>
